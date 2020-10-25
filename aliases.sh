e66a43bfa35b83e1135b068e3f1996bf1a752046
RubyHuntsman <PublicProgramer@gmx.com>
2020-10-25 22:11:39 +0100

    First aliases

---
+++ aliases.sh
@@ -0,0 +1,9 @@
+# System
+alias up="sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y"
+alias ainst="sudo apt install "
+alias apur="sudo apt purge"
+
+# Ruby on Rails development
+alias rs="rails s"
+alias rdm="rails db:migrate"
+alias rds="rails db:seed"
