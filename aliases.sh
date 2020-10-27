210f9d56887f99fa3660a0ef9154f06ecf4fe033
RubyHuntsman <PublicProgramer@gmx.com>
2020-10-27 23:41:34 +0100

    Add aliases to files and useful groups

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,18 @@ alias up="sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y"
 alias ainst="sudo apt install "
 alias apur="sudo apt purge"
 
+# Files 
+alias untar='tar -zxvf '
+alias sha='shasum -a 256 '
+alias wget='wget -c '
+
+# Usefull
+alias c='clear'
+alias h='history'
+alias speed='speedtest-cli --server 2406 --simple'
+alias df='df -H'
+alias du='du -ch'
+
 # Ruby on Rails development
 alias rs="rails s"
 alias rdm="rails db:migrate"
